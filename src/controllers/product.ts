4b5f4245557a72a47db4fb43eb8c53c84b10d87c
Sachin George Thomas <sachq1@gmail.com>
2020-11-14 01:29:40 -0500

    call getProduct method in the controller

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express';
 
+import { productModel } from '../models/product';
+import { Product } from '../models/product.interface';
+
 export class ProductController {
   public create(req: Request, res: Response): void {
     throw new Error("Method not implemented.");
   }
 
   public read(req: Request, res: Response): void {
-    res.json({ message: 'GET /user request received' });
+    const result = (req.params.id) ? productModel.getProduct(+req.params.id)
+                                   : productModel.products;
+    res.status(result ? 200 : 404)
+       .json({
+         content: result
+        });
   }
 
   public update(req: Request, res: Response): void {
