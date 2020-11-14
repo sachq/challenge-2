ad5016e09fd88c1fc5db709c0f1fd071663915f4
Sachin George Thomas <sachq1@gmail.com>
2020-11-14 01:27:23 -0500

    keep data separate

---
+++ src/_data/products.ts
@@ -0,0 +1,14 @@
+import { Product } from "../models/product.interface";
+
+const PRODUCTS: Product[] = [{"id":1,"name":"Transcof","cost":10.10,"currency":"Dollar","app_version":"0.49","country":"United States"},
+{"id":2,"name":"Konklab","cost":58.10,"currency":"Dollar","app_version":"7.3","country":"United States"},
+{"id":3,"name":"Cardify","cost":11.48,"currency":"Dollar","app_version":"9.8","country":"Canada"},
+{"id":4,"name":"Temp","cost":10.97,"currency":"Dollar","app_version":"0.9.7","country":"United States"},
+{"id":5,"name":"Stim","cost":20.50,"currency":"Dollar","app_version":"0.7.3","country":"United States"},
+{"id":6,"name":"Home Ing","cost":25.57,"currency":"Dollar","app_version":"4.49","country":"Canada"},
+{"id":7,"name":"Voyatouch","cost":47.93,"currency":"Dollar","app_version":"3.30","country":"United States"},
+{"id":8,"name":"Opela","cost":10.92,"currency":"Dollar","app_version":"0.7.8","country":"United States"},
+{"id":9,"name":"Biodex","cost":54.96,"currency":"Dollar","app_version":"0.98","country":"United States"},
+{"id":10,"name":"Quo Lux","cost":32.52,"currency":"Dollar","app_version":"0.1.6","country":"United States"}];
+
+export { PRODUCTS };
