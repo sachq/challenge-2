171ecebadccd9f8927d65b6de7f234498e890ee1
Sachin George Thomas <sachq1@gmail.com>
2020-11-14 00:37:08 -0500

    update data list in the product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -14,36 +14,36 @@ class ProductModel {
         country: "United States"
       },
       {
-        "id": 2,
-        "name":"Konklab",
-        "cost":"58.10",
-        "currency":"Dollar",
-        "app_version":"7.3",
-        "country":"United States"
+        id: 2,
+        name: "Konklab",
+        cost: "58.10",
+        currency: "Dollar",
+        app_version: "7.3",
+        country: "United States"
       },
       {
-        "id": 3,
-        "name": "Cardify",
-        "cost": "11.48",
-        "currency": "Dollar",
-        "app_version": "9.8",
-        "country":" Canada"
+        id: 3,
+        name: "Cardify",
+        cost: "11.48",
+        currency: "Dollar",
+        app_version: "9.8",
+        country:" Canada"
       },
       {
-        "id": 4,
-        "name": "Temp",
-        "cost": "10.97",
-        "currency": "Dollar",
-        "app_version": "0.9.7",
-        "country": "United States"
+        id: 4,
+        name: "Temp",
+        cost: "10.97",
+        currency: "Dollar",
+        app_version: "0.9.7",
+        country: "United States"
       },
       {
-        "id": 5,
-        "name": "Stim",
-        "cost": "20.50",
-        "currency": "Dollar",
-        "app_version": "0.7.3",
-        "country":"United States"
+        id: 5,
+        name: "Stim",
+        cost: "20.50",
+        currency: "Dollar",
+        app_version: "0.7.3",
+        country:"United States"
       }
     ]
   }
